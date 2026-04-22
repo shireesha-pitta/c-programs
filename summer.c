33c32fe0dda3c4f74193e63956f141c1bc9f61b2
shireesha-pitta <pittasirisha00@gmail.com>
2026-04-22 16:23:49 +0530

    Add files via upload

---
+++ summer.c
@@ -0,0 +1,27 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() 
+{
+  int temp;
+  printf("enter the temperature:\n");
+  scanf("%d",&temp);
+  int summer;
+  printf("enter 1 for summer and 0 for not_summer:\n");
+  scanf("%d",&summer);
+  if(summer==1)
+  {
+      if (temp>=60 && temp<=100)
+          printf("true");
+      else
+      printf("false");
+  }
+  else
+  {
+          if(temp>=60 && temp<=90)
+              printf("true");
+          else
+              printf("false");
+  }
+  return 0;
+}
